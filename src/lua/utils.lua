79da57fe056186b26b7733ef18b9e1e42d4d6b9a
S1M0N38 <bertolottosimone@gmail.com>
2025-07-09 11:49:06 +0200

    feat: add blind_on_deck to game_state and ftm code

--- src/lua/utils.lua
+++ src/lua/utils.lua
@@ -2,97 +2,98 @@ utils = {}
 local json = require("json")
 
 function utils.get_game_state()
-	local game = nil
-	if G.GAME then
-		game = {
-			hands_played = G.GAME.hands_played,
-			skips = G.GAME.Skips,
-			round = G.GAME.round,
-			discount_percent = G.GAME.discount_percent,
-			interest_cap = G.GAME.interest_cap,
-			inflation = G.GAME.inflation,
-			dollars = G.GAME.dollars,
-			max_jokers = G.GAME.max_jokers,
-			bankrupt_at = G.GAME.bankrupt_at,
-			chips = G.GAME.chips,
-			current_round = {
-				discards_left = G.GAME.current_round.discards_left,
-			},
-		}
-	end
+  local game = nil
+  if G.GAME then
+    game = {
+      hands_played = G.GAME.hands_played,
+      skips = G.GAME.Skips,
+      round = G.GAME.round,
+      discount_percent = G.GAME.discount_percent,
+      interest_cap = G.GAME.interest_cap,
+      inflation = G.GAME.inflation,
+      dollars = G.GAME.dollars,
+      max_jokers = G.GAME.max_jokers,
+      bankrupt_at = G.GAME.bankrupt_at,
+      chips = G.GAME.chips,
+      blind_on_deck = G.GAME.blind_on_deck,
+      current_round = {
+        discards_left = G.GAME.current_round.discards_left,
+      },
+    }
+  end
 
-	local hand = nil
-	if G.hand then
-		hand = {}
-		for i, card in pairs(G.hand.cards) do
-			hand[i] = {
-				label = card.label,
-				config = {
-					card = {
-						name = card.config.card.name,
-						suit = card.config.card.suit,
-						value = card.config.card.value,
-						card_key = card.config.card_key,
-					},
-				},
-			}
-		end
-	end
+  local hand = nil
+  if G.hand then
+    hand = {}
+    for i, card in pairs(G.hand.cards) do
+      hand[i] = {
+        label = card.label,
+        config = {
+          card = {
+            name = card.config.card.name,
+            suit = card.config.card.suit,
+            value = card.config.card.value,
+            card_key = card.config.card_key,
+          },
+        },
+      }
+    end
+  end
 
-	local jokers = {}
-	if G.jokers and G.jokers.cards then
-		for i, card in pairs(G.jokers.cards) do
-			jokers[i] = {
-				label = card.label,
-				config = {
-					center = card.config.center,
-				},
-			}
-		end
-	end
+  local jokers = {}
+  if G.jokers and G.jokers.cards then
+    for i, card in pairs(G.jokers.cards) do
+      jokers[i] = {
+        label = card.label,
+        config = {
+          center = card.config.center,
+        },
+      }
+    end
+  end
 
-	-- TODO: add consumables, ante, and shop
+  -- TODO: add consumables, ante, and shop
 
-	return {
-		state = G.STATE,
-		game = game,
-		hand = hand,
-		jokers = jokers,
-	}
+  return {
+    state = G.STATE,
+    game = game,
+    hand = hand,
+    jokers = jokers,
+  }
 end
 
 function utils.table_to_json(obj, depth)
-	depth = depth or 3
+  depth = depth or 3
 
-	local function sanitize_for_json(value, current_depth)
-		if current_depth <= 0 then
-			return "..."
-		end
+  local function sanitize_for_json(value, current_depth)
+    if current_depth <= 0 then
+      return "..."
+    end
 
-		local value_type = type(value)
+    local value_type = type(value)
 
-		if value_type == "nil" then
-			return nil
-		elseif value_type == "string" or value_type == "number" or value_type == "boolean" then
-			return value
-		elseif value_type == "function" then
-			return "function"
-		elseif value_type == "userdata" then
-			return "userdata"
-		elseif value_type == "table" then
-			local sanitized = {}
-			for k, v in pairs(value) do
-				local key = type(k) == "string" and k or tostring(k)
-				sanitized[key] = sanitize_for_json(v, current_depth - 1)
-			end
-			return sanitized
-		else
-			return tostring(value)
-		end
-	end
+    if value_type == "nil" then
+      return nil
+    elseif value_type == "string" or value_type == "number" or value_type == "boolean" then
+      return value
+    elseif value_type == "function" then
+      return "function"
+    elseif value_type == "userdata" then
+      return "userdata"
+    elseif value_type == "table" then
+      local sanitized = {}
+      for k, v in pairs(value) do
+        local key = type(k) == "string" and k or tostring(k)
+        sanitized[key] = sanitize_for_json(v, current_depth - 1)
+      end
+      return sanitized
+    else
+      return tostring(value)
+    end
+  end
 
-	local sanitized = sanitize_for_json(obj, depth)
-	return json.encode(sanitized)
+  local sanitized = sanitize_for_json(obj, depth)
+  return json.encode(sanitized)
 end
 
 return utils
