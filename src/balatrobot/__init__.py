001dc0e6a8feca2f23ea9f94eb26592be370055e
S1M0N38 <bertolottosimone@gmail.com>
2025-07-19 10:23:22 +0200

    chore(main): release 0.5.1

--- src/balatrobot/__init__.py
+++ src/balatrobot/__init__.py
@@ -5,7 +5,7 @@ from .enums import Actions, Decks, Stakes, State
 from .exceptions import BalatroError
 from .models import GameState
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __all__ = [
     # Main client
     "BalatroClient",
