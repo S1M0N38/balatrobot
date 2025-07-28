a91cb68547521dfe38e381a81288d913c9afba8c
S1M0N38 <bertolottosimone@gmail.com>
2025-07-28 10:59:44 +0200

    test: add parallel tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,35 @@ def pytest_addoption(parser):
     """Add command line options for pytest."""
     parser.addoption(
         "--port",
-        action="store",
-        default=12346,
+        action="append",
         type=int,
-        help="Port number for TCP connection (default: 12346)",
+        help="Port number for Balatro instance (use multiple times for parallel execution)",
     )
 
 
+def pytest_configure(config):
+    """Configure ports and distribution."""
+    ports = config.getoption("--port") or [12346]
+
+    # Remove duplicates while preserving order
+    unique_ports = []
+    for port in ports:
+        if port not in unique_ports:
+            unique_ports.append(port)
+
+    config._balatro_ports = unique_ports
+
+    if len(unique_ports) > 1:
+        config.option.dist = "loadscope"
+
+
 @pytest.fixture(scope="session")
-def port(request):
-    """Get the port number from command line option."""
-    return request.config.getoption("--port")
+def port(request, worker_id):
+    """Get assigned port for this worker."""
+    ports = getattr(request.config, "_balatro_ports", [12346])
+
+    if worker_id == "master":
+        return ports[0]
+
+    worker_num = int(worker_id.replace("gw", ""))
+    return ports[worker_num % len(ports)]
